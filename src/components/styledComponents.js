bd90b70288654cd7c96a3eee3b7eda0a1d48f0e1
zzz664 <temperis0512@gmail.com>
2025-01-25 11:11:26 +0900

    feat(styledComponent): add Styled div tag contentDiv -> outer div sectionDiv -> inner div

---
+++ src/components/styledComponents.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const contentDiv = styled.div`
+  height: 100vh;
+  overflow-y: auto;
+  scrollbar-width: none;
+`;
+
+export const sectionDiv = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 100px;
+  background-color: ${(props) => props.background || 'white'};
+`;
