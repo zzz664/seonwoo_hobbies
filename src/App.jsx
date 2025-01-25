338fcf1091560f4ff5b4d64311602733bbec4db7
zzz664 <temperis0512@gmail.com>
2025-01-25 11:10:06 +0900

    feat(App.jsx): add Page Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react';
 import './App.css';
+import { useState } from 'react';
+import Navigator from './components/layouts/Navigator.jsx';
+import * as Styled from './components/styledComponents.js';
+import Home from './components/pages/Home.jsx';
+import Introduce from './components/pages/Introduce.jsx';
+import Values from './components/pages/Values.jsx';
+import Projects from './components/pages/Projects.jsx';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>seonwoo</div>
+      <Navigator></Navigator>
+      <Styled.contentDiv>
+        <Home></Home>
+        <Introduce></Introduce>
+        <Values></Values>
+        <Projects></Projects>
+      </Styled.contentDiv>
     </>
   );
 }
