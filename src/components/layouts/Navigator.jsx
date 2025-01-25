ced1d43b8964657b3a2e27cfe435aec93760e513
zzz664 <temperis0512@gmail.com>
2025-01-25 11:12:43 +0900

    chore(Navigator): add fixed props

--- src/components/layouts/Navigator.jsx
+++ src/components/layouts/Navigator.jsx
@@ -4,11 +4,11 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 function Navigator() {
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar bg="dark" data-bs-theme="dark" fixed="top">
         <Container>
           <Navbar.Brand href="#home">Seonwoo's Hobbies</Navbar.Brand>
           <Nav className="ms-auto">
-            <Nav.Link href="#home">home</Nav.Link>
+            <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#introduce">Introduce</Nav.Link>
             <Nav.Link href="#values">Values</Nav.Link>
             <Nav.Link href="#projects">Projects</Nav.Link>
